88e41695a4619f4fefc5acc10e8a875088d6251b
Jang Hyeon Jeong <88546743+JangGusWjd@users.noreply.github.com>
2023-07-21 23:37:49 +0900

    Update App.js&index.js: App.js에 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import "./styles/App.scss";
+import { CampaignProvider } from "./components/CampaignContext";
+import NavBar from "./components/Navbar";
+import MainApp from "./pages/MainPage/MainApp";
+import RegisterApp from "./pages/RegisterPage/RegisterApp";
+import ProfileApp from "./pages/ProfilePage/ProfileApp";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route
+          element={
+            <CampaignProvider>
+              <NavBar />
+            </CampaignProvider>
+          }
+        >
+          <Route path="/" element={<MainApp />} />
+          <Route path="/register" element={<RegisterApp />} />
+          <Route path="/profile" element={<ProfileApp />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
