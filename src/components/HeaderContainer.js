2d44517a1bcedb12fb84a3b19c0767aec1bd079a
Jang Hyeon Jeong <88546743+JangGusWjd@users.noreply.github.com>
2023-07-26 00:56:03 +0900

    Added HeaderContainer.js : 파일마다 중복된 스타일 컴포넌트로 인해 상단 Header에 대해 재사용 가능한 스타일 컴포넌트 생성

---
+++ src/components/HeaderContainer.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  max-width: 375px;
+  height: 3rem;
+  background: white;
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+`;
