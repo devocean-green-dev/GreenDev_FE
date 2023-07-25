8b418bebb87931471662b95698905e19d7916348
Jang Hyeon Jeong <88546743+JangGusWjd@users.noreply.github.com>
2023-07-26 00:51:46 +0900

    Update App.js : 캠페인 참여게시판 리스트 페이지 이동 경로 & 참여 인증글 CommentContextProvider 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import "./styles/App.scss";
 import { CampaignProvider } from "./components/CampaignContext";
+import { CommentContextProvider } from "./components/CampaignComment";
 import NavBar from "./components/Navbar";
 import MainApp from "./pages/MainPage/MainApp";
 import RegisterApp from "./pages/RegisterPage/RegisterApp";
 import SearchApp from "./pages/SearchPage/SearchApp";
 import CampaignDetail from "./pages/SearchPage/CampaignDetail";
+import CampaignAuthList from "./pages/SearchPage/CampaignAuthList";
 import ProfileApp from "./pages/ProfilePage/ProfileApp";
 
 function App() {
@@ -23,6 +25,14 @@ function App() {
           <Route path="/register" element={<RegisterApp />} />
           <Route path="/search" element={<SearchApp />} />
           <Route path="/campaigns/:campaignId" element={<CampaignDetail />} />
+          <Route
+            path="/campaigns/:campaignId/participations"
+            element={
+              <CommentContextProvider>
+                <CampaignAuthList />
+              </CommentContextProvider>
+            }
+          />
           <Route path="/profile" element={<ProfileApp />} />
         </Route>
       </Routes>
