6d4ab082651c7d5911d3c30e290ac727f9b933d6
Jang Hyeon Jeong <88546743+JangGusWjd@users.noreply.github.com>
2023-07-28 23:08:24 +0900

    Added CampaignSummary.js : 인증할 캠페인 정보를 요약해서 보여주는 컴포넌트 작성

---
+++ src/pages/SearchPage/CampaignSummary.js
@@ -0,0 +1,20 @@
+// 캠페인 인증 글 작성시 상단에 인증하고자 하는 캠페인 정보 요약하여 보여주기
+import "../../styles/SearchPage/CampaignSummary.scss";
+
+const CampaignSummary = ({ imageUrl, date, title, company, description }) => {
+  return (
+    <div className="campaign-summary">
+      <div className="campaign-img">
+        <img src={imageUrl} alt="Campaign" />
+      </div>
+      <div className="campaign-description">
+        <p>{date}</p>
+        <p>{title}</p>
+        <p>{company}</p>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CampaignSummary;
