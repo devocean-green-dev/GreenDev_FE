422b3ff03549765c2f32b6ad8a739fd75aac2b68
Jang Hyeon Jeong <88546743+JangGusWjd@users.noreply.github.com>
2023-08-04 00:09:02 +0900

    Update ProfileApp.js : 프로필 페이지 컴포넌트에 header 및 프로필 꾸미기 컴포넌트 불러오기

--- src/pages/ProfilePage/ProfileApp.js
+++ src/pages/ProfilePage/ProfileApp.js
@@ -1,5 +1,16 @@
+import { HeaderContainer } from "../../components/HeaderContainer";
+import Header from "../../components/Header";
+import ProfileCustomize from "./ProfileCustomize";
+
 const ProfileApp = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div>
+      <HeaderContainer>
+        <Header menuTitle="프로필" />
+      </HeaderContainer>
+      <ProfileCustomize />
+    </div>
+  );
 };
 
 export default ProfileApp;
